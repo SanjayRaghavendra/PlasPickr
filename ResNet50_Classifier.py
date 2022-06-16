ef40b6172b55cd3f84594d33a7d51c06e468d35a
Sanjay R <sanjay14112000@gmail.com>
2022-06-16 22:50:54 +0530

    train-test code

---
+++ ResNet50_Classifier.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import pathlib
+
+path='Plastic_data'
+data_dir = pathlib.Path(path)
+
+img_height,img_width=180,180
+batch_size=32
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+resnet_model = Sequential()
+
+pretrained_model= tf.keras.applications.ResNet50(include_top=False,
+                   input_shape=(180,180,3),
+                   pooling='avg',classes=7,
+                   weights='imagenet')
+for layer in pretrained_model.layers:
+        layer.trainable=False
+
+resnet_model.add(pretrained_model)
+resnet_model.add(Flatten())
+resnet_model.add(Dense(512, activation='relu'))
+resnet_model.add(Dense(7, activation='softmax'))
+
+resnet_model.summary()
+
+resnet_model.compile(optimizer=Adam(lr=0.001),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+epochs=10
+history = resnet_model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+fig1 = plt.gcf()
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.axis(ymin=0.4,ymax=1)
+plt.grid()
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'validation'])
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.grid()
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'validation'])
+plt.show()
+
+resnet_model.save("resnet50_classifier")
+
---
+++ vgg19.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,Flatten
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+import pathlib 
+from pathlib import Path
+
+path='Plastic_Data/'
+data_dir = pathlib.Path(path)
+
+img_height,img_width=180,180
+batch_size=32
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+vgg_model = Sequential()
+
+pretrained_model= tf.keras.applications.vgg19.VGG19(
+    include_top=False,
+    weights='imagenet',
+    input_shape=(180,180,3),
+    classes=7,
+    classifier_activation='softmax'
+)
+
+for layer in pretrained_model.layers:
+        layer.trainable=False
+
+vgg_model.add(pretrained_model)
+vgg_model.add(Flatten())
+vgg_model.add(Dense(512, activation='relu'))
+vgg_model.add(Dense(7, activation='softmax'))
+
+vgg_model.compile(optimizer=Adam(lr=0.001),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+epochs=10
+history = vgg_model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+fig1 = plt.gcf()
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.axis(ymin=0.4,ymax=1)
+plt.grid()
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(['train', 'validation'])
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.grid()
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epochs')
+plt.legend(['train', 'validation'])
+plt.show()
+
+vgg_model.save("vgg19_classifier")
+
